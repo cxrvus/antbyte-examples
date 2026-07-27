178d653ed0ffb8684814db832a2c6efb478622f2
cxrvus <benito.m.rabe@gmail.com>
2026-07-27 19:12:37 +0200

    misc comp_json

--- comp_json.sh
+++ comp_json.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 find . -name "*.ant" -type f ! -path "./lib/*" | sort | while read -r file; do
+	echo -e "\nCompiling File $file"
 	antbyte "$file" -j > "$file".json # 2>> tmp/test_log.txt
 done
